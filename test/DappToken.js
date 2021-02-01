f4b389cec615f190703bf46da6c69932ff9058fe
okcodes <ok@ok.codes>
2021-02-01 00:04:40 -0600

    Simplify tests

--- test/DappToken.js
+++ test/DappToken.js
@@ -1,11 +1,9 @@
 const DappToken = artifacts.require("DappToken");
 
 contract('DappToken', accounts => {
-    let tokenInstance;
-    it('sets the total supply upon deployment', () => DappToken.deployed().then(instance => {
-        tokenInstance = instance;
-        return tokenInstance.totalSupply();
-    }).then(function (totalSupply) {
-        assert.equal(totalSupply.toNumber(), 1000000, 'sets the total suply to 1M');
-    }));
+  it('sets the total supply upon deployment', async () => {
+    const tokenInstance = await DappToken.deployed();
+    const totalSupply = await tokenInstance.totalSupply();
+    assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to 1M');
+  });
 });
