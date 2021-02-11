305603135123caf628326221709a75154f32f2b6
okcodes <ok@ok.codes>
2021-02-11 00:11:28 -0600

    11: Format code and add docs

--- test/DappToken.test.js
+++ test/DappToken.test.js
@@ -36,10 +36,10 @@ contract('DappToken', async accounts => {
       });
 
 
-    const success = await tokenInstance.transfer.call(accounts[1], 250_000, { from: accounts[0] });
+    const success = await tokenInstance.transfer.call(accounts[1], 250_000, {from: accounts[0]});
     assert.equal(success, true, 'it returns true');
 
-    const receipt = await tokenInstance.transfer(accounts[1], 250_000, { from: accounts[0] });
+    const receipt = await tokenInstance.transfer(accounts[1], 250_000, {from: accounts[0]});
 
     assert.equal(receipt.logs.length, 1, 'triggers one event');
     assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
@@ -62,7 +62,7 @@ contract('DappToken', async accounts => {
     // Note how then actually calling the function, we don't get just the returned value but a 'receipt?' object.
 
     // actually calling 'approve' (calling it directly without using the 'call' function)
-    const receipt = await tokenInstance.approve(accounts[1], 100, { from: accounts[0] });
+    const receipt = await tokenInstance.approve(accounts[1], 100, {from: accounts[0]});
     assert.equal(receipt.logs.length, 1, 'triggers one event');
     assert.equal(receipt.logs[0].event, 'Approval', 'should be the "Approval" event');
     assert.equal(receipt.logs[0].args._owner, accounts[0], 'logs the account the tokens are authorized by');
@@ -79,22 +79,22 @@ contract('DappToken', async accounts => {
     const spendingAccount = accounts[4];
 
     // Transfer some tokens to fromAccount (minter grants 100 to fromAccount)
-    const transferReceipt = await tokenInstance.transfer(fromAccount, 100, { from: accounts[0] });
+    const transferReceipt = await tokenInstance.transfer(fromAccount, 100, {from: accounts[0]});
     // Approve spendinAccount to spend 10 tokens from fromAccount (fromAccount allows spendingAccount to spend 10 tokens)
-    const approveReceipt = await tokenInstance.approve(spendingAccount, 10, { from: fromAccount });
+    const approveReceipt = await tokenInstance.approve(spendingAccount, 10, {from: fromAccount});
 
     // Try transferring something larger than the sender's balance
-    await assertThrowsAsync(() => tokenInstance.transferFrom(fromAccount, toAccount, 9_999, { from: spendingAccount }), /revert/, 'cannot transfer values larger than balance');
+    await assertThrowsAsync(() => tokenInstance.transferFrom(fromAccount, toAccount, 9_999, {from: spendingAccount}), /revert/, 'cannot transfer values larger than balance');
 
     // Try transferring something larger than the approved ammount
-    await assertThrowsAsync(() => tokenInstance.transferFrom(fromAccount, toAccount, 20, { from: spendingAccount }), /revert/, 'cannot transfer values larger than approved ammount');
+    await assertThrowsAsync(() => tokenInstance.transferFrom(fromAccount, toAccount, 20, {from: spendingAccount}), /revert/, 'cannot transfer values larger than approved ammount');
 
     // Simulated call
-    const success = await tokenInstance.transferFrom.call(fromAccount, toAccount, 10, { from: spendingAccount });
+    const success = await tokenInstance.transferFrom.call(fromAccount, toAccount, 10, {from: spendingAccount});
     assert.equal(success, true);
 
     // Actual call
-    const receipt = await tokenInstance.transferFrom(fromAccount, toAccount, 10, { from: spendingAccount });
+    const receipt = await tokenInstance.transferFrom(fromAccount, toAccount, 10, {from: spendingAccount});
     assert.equal(receipt.logs.length, 1, 'triggers one event');
     assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
     assert.equal(receipt.logs[0].args._from, fromAccount, 'logs the account the tokens are transfered from');
@@ -112,12 +112,25 @@ contract('DappToken', async accounts => {
   })
 });
 
+/**
+ * Asserts that the given function returns a promise that throws using the chai assertion library.
+ *
+ * The assertion will succeed if calling 'fn' returns a promise that throws an error. The assertion will fail otherwise.
+ *
+ * @see {@link https://www.chaijs.com/api/assert/#method_throws|Chai Docs} for further information.
+ *
+ * @param {function(): Promise<any>} fn - Function that returns the promise which will be asserted to throw.
+ * @param {RegExp|String} errMsgMatcher - If provided, it also asserts that the error thrown will have a message matching errMsgMatcher.
+ * @param {String} msg - Message to display on error.
+ * @returns {Promise<void>}
+ */
 const assertThrowsAsync = async (fn, errMsgMatcher, msg = '') => {
   try {
     await fn();
-  }
-  catch (error) {
-    assert.throws(() => { throw error; }, error, errMsgMatcher, msg);
+  } catch (error) {
+    assert.throws(() => {
+      throw error;
+    }, error, errMsgMatcher, msg);
     // At this point if assert didn't throw, return to make the assert to pass.
     return;
   }
